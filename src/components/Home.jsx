a38378a6d21fb7bab2ae60bd3f7246bb06a70b42
Terry <shiyuntian@126.com>
2023-07-17 14:08:13 -0400

    improve display

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 function Home() {
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      <div className=' flex flex-col z-10'>
+      <div className='px-10 flex flex-col z-10'>
         <div className='flex flex-col justify-end'>
           <div className='text-3xl font-bold w-fit text-slate-500 hover:text-slate-600 transition-colors duration-500'>
             TERRY SHI
